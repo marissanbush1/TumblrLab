e370112fe2de9cd5c10571fc621bd818846f34e6
Marissa Bush <marissabush@Marissas-MacBook-Pro.local>
2017-11-17 14:49:41 -0500

    completely forgot to commit while I was doing Tumblr

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TumblrLab' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TumblrLab
+  pod 'AlamofireImage'
+  # Add additional dependencies
+  
+
+  target 'TumblrLabTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TumblrLabUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ TumblrLab/PhotoCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCell.swift
+//  TumblrLab
+//
+//  Created by Marissa Bush on 11/17/17.
+//  Copyright © 2017 Marissa Bush. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
